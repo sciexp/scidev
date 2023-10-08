7fe8663f907291c641a740ee6ec5171e7f025545
Cameron Smith <cameron.ray.smith@gmail.com>
2023-10-08 02:30:04 -0400

    feat: add pipeline export script Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

---
+++ scripts/export-stack.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+set -x
+
+#------
+# setup
+#------
+
+zenml connect \
+  --url "${ZENML_SERVER_URL}" \
+  --username "${ZENML_SERVER_USERNAME}" \
+  --password "${ZENML_SERVER_PASSWORD}"
+zenml stack set "${ZENML_STACK_NAME}"
+zenml status
+
+#---------------------------
+# export stack configuration
+#---------------------------
+
+zenml service-connector describe kubeflow-cluster
+zenml service-connector describe gcp-multi-type
+zenml container-registry describe gcp-registry
+zenml artifact-store describe gcp-store
+zenml image-builder describe kaniko
+zenml orchestrator describe kubeflow
+zenml stack describe "${ZENML_STACK_NAME}"
+
+zenml stack export "${ZENML_STACK_NAME}" stack-config.yaml
